b6c171e398171697b820669a4568960ccd20cf89
AndrewFox_DEVELOPER <114300206+AndrewFox-DEV@users.noreply.github.com>
2025-07-02 14:44:47 +0200

    Update visibility.py
    protected decorator added

--- oop_decorators/visibility.py
+++ oop_decorators/visibility.py
@@ -1,5 +1,6 @@
 from functools import wraps
-from typing import Any, cast
+from typing import Any, Callable, cast
+import inspect
 
 def private(method: function) -> function:
   @wraps(method)
@@ -10,6 +11,22 @@ def private(method: function) -> function:
 
   wrapper.__visibility__ = 'private'
   return cast(function, wrapper)
+
+def protected(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self: Any, *args: Any, **kwargs: Any) -> Any:
+        stack = inspect.stack()
+        for frame_info in stack[1:]:
+            caller_locals = frame_info.frame.f_locals
+            caller_self = caller_locals.get('self', None)
+            if isinstance(caller_self, self.__class__) or isinstance(self, caller_self.__class__):
+                break
+        else:
+            raise AttributeError(f'Method {method.__name__} is protected and cannot be accessed externally.')
+        return method(self, *args, **kwargs)
+
+    wrapper.__visibility__ = 'protected'
+    return cast(Callable, wrapper)
 
 def public(method: function) -> function:
   method.__visibility__ = 'public'
